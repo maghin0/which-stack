06e8d941342d4cb61d213f2cfdfdef52922e0aa6
Maghin <ahmedshabana0.0@gmail.com>
2019-08-29 11:16:22 +0200

    Finished (subs, post likes/unlikes, comments,etc.)

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,12 @@
-const Post = require("../../models/Post");
+const { AuthenticationError } = require("apollo-server");
 
+const Post = require("../../models/Post");
+const checkAuth = require("../../utilities/check-auth");
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -24,6 +26,58 @@ module.exports = {
     }
   },
   Mutation: {
-    async createPost(_, { body }) {}
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user);
+      const newPost = new Post({
+        body,
+        user: user.indexOf,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      });
+      const post = await newPost.save();
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (user.username === post.username) {
+          await post.delete();
+          return "post deleted";
+        } else {
+          throw new AuthenticationError("not creator of the post");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          // Post already likes, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          // Not liked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST")
+    }
   }
 };
